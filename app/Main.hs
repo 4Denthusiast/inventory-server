db8fabba73b3826f63284048ea3288d54a3ad235
4Denthusiast <25589515+4Denthusiast@users.noreply.github.com>
2023-09-14 18:50:45 +0100

    Add an option to control the directory used.
    Since the program needs to know where the files for the pages are,
    and when it's started by systemd it won't get the correct working
    directory, you can now change that. I could have just used a separate
    shell script for this, but this way was easier.

--- app/Main.hs
+++ app/Main.hs
@@ -3,10 +3,13 @@ module Main (main) where
 import Lib
 
 import System.Environment
+import System.Posix.Directory
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
     [] ->  startAppNet
-    (addr:_) -> startAppSocket addr
+    [addr] -> startAppSocket addr
+    [addr,dir] -> changeWorkingDirectory dir >> startAppSocket addr
+    _ -> error "Too many arguments."
